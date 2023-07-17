a63bc6f9fae8fd3ed78b09ee1415c8c5e6ed2233
rehanakabirmim <rehanakabirmim@gmail.com>
2023-07-18 01:38:05 +0600

    first commit

---
+++ resources/views/includes/copyright.blade.php
@@ -0,0 +1,3 @@
+<div class="copyright py-4 text-center text-white">
+            <div class="container"><small>Copyright &copy; Your Laravel {{date('Y')}}</small></div>
+        </div>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+        @include('includes.css');
+        <!-- Navigation-->
+        @include('includes.navbar');
+       
+        <!-- Masthead-->
+        @include('includes.header');
+        <!-- Portfolio Section-->
+        @include('includes.portfolio');
+        <!-- About Section-->
+        @include('includes.about');
+        <!-- Contact Section-->
+        @include('includes.contact');
+        <!-- Footer-->
+        @include('includes.footer');
+        <!-- Copyright Section-->
+        @include('includes.copyright');
+        <!-- Portfolio Modals-->
+       
+        @include('includes.portfolioModel');
+        <!-- Bootstrap core JS-->
+        @include('includes.js');
